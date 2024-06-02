e0579e7b0a7385df41ad7f96b68e0b7ce644045f
Nils Dammenhayn <nilsdam@aol.com>
2024-06-02 11:17:24 +0200

    refactoring to three input fields

--- src/ui/StopwatchValueContainer.tsx
+++ src/ui/StopwatchValueContainer.tsx
@@ -9,52 +9,78 @@ export type StopwachValueContainerProps = {
 const StopwachValueContainer = (props: StopwachValueContainerProps) => {
 	const TWO_DIGITS_REGEX = /^\d{0,2}$/;
 	const [isEditing, setIsEditing] = useState(false);
-	const [isBlur, setIsBlur] = useState(false);
-	const inputRef = useRef<HTMLInputElement>(null);
+	const [hours, setHours] = useState<number>(0);
+	const [minutes, setMinutes] = useState<number>(0);
+	const [seconds, setSeconds] = useState<number>(0);
+	const inputHoursRef = useRef<HTMLInputElement>(null);
 
 	useEffect(() => {
 		if (isEditing) {
-			inputRef.current?.focus();
+			inputHoursRef.current?.focus();
 		}
 	}, [isEditing]);
 
 	const handleOnButtonClick = () => {
-		if (!isBlur) {
+		if (isEditing) {
+			setIsEditing(false);
+		} else {
 			props.stopStopwatch();
 			setIsEditing(true);
-		} else {
-			setIsBlur(false);
+			initializeTimeValues();
 		}
 	};
 
-	const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		const newValue = event.target.value;
-		const cursorPosition = event.target.selectionStart;
+	const initializeTimeValues = () => {
+		const valueParts = props.stopwatchValue.split(':');
+		setHours(parseInt(valueParts[0]));
+		setMinutes(parseInt(valueParts[1]));
+		setSeconds(parseInt(valueParts[2]));
+	};
 
-		const valueParts = newValue.split(':');
-		const hoursString = createTimeString(valueParts[0]);
-		const minutesString = createTimeString(valueParts[1]);
-		const secondsString = createTimeString(valueParts[2]);
+	const handleOnHoursChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const hours = prepareValue(event.target.value);
 
-		if (!(hoursString && minutesString && secondsString)) {
-			return;
-		}
-
-		const hours = parseInt(hoursString);
-		const minutes = parseInt(minutesString);
-		const seconds = parseInt(secondsString);
-
-		if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59 || seconds < 0 || seconds > 59) {
+		if (!hours || hours < 0 || hours > 23) {
 			return;
 		}
 
 		const date = new Date();
 		date.setHours(date.getHours() - hours, date.getMinutes() - minutes, date.getSeconds() - seconds);
 		props.setStopwatchValue(date.getTime());
+	};
 
-		if (cursorPosition !== undefined) {
-			event.target.setSelectionRange(cursorPosition, cursorPosition);
+	const handleOnMinutesChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const minutes = prepareValue(event.target.value);
+
+		if (!minutes || minutes < 0 || minutes > 59) {
+			return;
 		}
+
+		const date = new Date();
+		date.setHours(date.getHours() - hours, date.getMinutes() - minutes, date.getSeconds() - seconds);
+		props.setStopwatchValue(date.getTime());
+	};
+
+	const handleOnSecondsChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const seconds = prepareValue(event.target.value);
+
+		if (!seconds || seconds < 0 || seconds > 59) {
+			return;
+		}
+
+		const date = new Date();
+		date.setHours(date.getHours() - hours, date.getMinutes() - minutes, date.getSeconds() - seconds);
+		props.setStopwatchValue(date.getTime());
+	};
+
+	const prepareValue = (newValue: string): null | number => {
+		const secondsString = createTimeString(newValue);
+
+		if (!secondsString) {
+			return null;
+		}
+
+		return parseInt(secondsString);
 	};
 
 	const createTimeString = (timeString: string): string | null => {
@@ -68,21 +94,31 @@ const StopwachValueContainer = (props: StopwachValueContainerProps) => {
 		return timeString;
 	};
 
-	const handleOnBlur = () => {
-		setIsBlur(true);
-		setIsEditing(false);
-	};
-
 	return (
 		<div className="stopwatch-value-container">
 			<input
-				ref={inputRef}
+				ref={inputHoursRef}
 				type="text"
 				disabled={!isEditing}
-				className="stopwatch-value-input custom-input"
-				value={props.stopwatchValue}
-				onChange={handleOnChange}
-				onBlur={handleOnBlur}
+				className="stopwatch-value-input"
+				value={props.stopwatchValue.split(':')[0]}
+				onChange={handleOnHoursChange}
+			/>
+			<p>:</p>
+			<input
+				type="text"
+				disabled={!isEditing}
+				className="stopwatch-value-input"
+				value={props.stopwatchValue.split(':')[1]}
+				onChange={handleOnMinutesChange}
+			/>
+			<p>:</p>
+			<input
+				type="text"
+				disabled={!isEditing}
+				className="stopwatch-value-input"
+				value={props.stopwatchValue.split(':')[2]}
+				onChange={handleOnSecondsChange}
 			/>
 
 			<button className="edit-button" onClick={handleOnButtonClick}>
