6bd53caf96fc09ba2878ebe12274760ebdbad79f
Nils Dammenhayn <nilsdam@aol.com>
2025-12-17 21:32:06 +0100

    don't instantly save on start

--- src/ui/TimetrackerView.tsx
+++ src/ui/TimetrackerView.tsx
@@ -65,7 +65,6 @@ export class TimetrackerView extends ItemView implements PersistentStopwatchStat
 
 	start(): StopwatchState {
 		const state = this.stopwatchModel.start();
-		this.app.workspace.requestSaveLayout();
 		return state;
 	}
 
