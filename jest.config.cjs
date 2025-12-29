1e6d775c83d406e5dc1333e2addee035f8ca61e7
Nils Dammenhayn <nilsdam@aol.com>
2025-12-29 10:08:36 +0100

    add jest coverage script

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,7 @@
 module.exports = {
 	testEnvironment: 'jsdom',
-	transform: { ".(ts|tsx)": "ts-jest" },
-};
+	transform: { '.(ts|tsx)': 'ts-jest' },
+	collectCoverage: true,
+	collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}', '!src/**/*.test.{ts,tsx,js,jsx}'],
+	coverageDirectory: 'coverage',
+}
