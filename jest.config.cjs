97b4d58f21039db0aa202ae85150eb35b68b3388
Nils Dammenhayn <nilsdam@aol.com>
2025-12-29 14:51:25 +0100

    deactivate coverage per default

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
 	testEnvironment: 'jsdom',
 	transform: { '.(ts|tsx)': 'ts-jest' },
-	collectCoverage: true,
+	collectCoverage: false,
 	collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}', '!src/**/*.test.{ts,tsx,js,jsx}'],
 	coverageDirectory: 'coverage',
 }
